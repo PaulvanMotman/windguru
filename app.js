7c7293d1a3ce3a7ad8887414f7d491bb1a68b2ff
Paul van Motman <pcvanmotman@gmail.com>
2016-06-26 19:51:59 +0200

    added resources folder and created the API call in the app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
+var sendgrid_username   = process.env.SENDGRID_USERNAME;
+var sendgrid_password   = process.env.SENDGRID_PASSWORD;
+var to                  = ['pcvanmotman@gmail.com', 'paulrules32@hotmail.com']
+
+
+var sendgrid   = require('sendgrid')(sendgrid_username, sendgrid_password);
+var email      = new sendgrid.Email();
+
+email.addTo(to);
+email.setFrom(to);
+email.setSubject('Good surf on its way!');
+email.setText('Check out the waves on Wednesday!');
+email.setHtml('<strong>Check out the waves on Wednesday!</strong>');
+email.addHeader('X-Sent-Using', 'SendGrid-API');
+email.addHeader('X-Transport', 'web');
+email.addFile({path: './resources/waves.png', filename: 'waves.png'});
+
+sendgrid.send(email, function(err, json) {
+  if (err) { return console.error(err); }
+  console.log(json);
+});
